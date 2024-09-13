f98ffad90254fe1fe002e39b23dcc168775bd0cf
Greg-Lim <limgreg33cc@gmail.com>
2024-09-13 13:57:55 +0800

    Fix constant.h

--- src/constants.h
+++ src/constants.h
@@ -2,30 +2,31 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int BLOCK_SIZE = 512;
-const int ENTRY_SIZE = sizeof(GameEntry) + sizeof(int); // 33 bytes
-const int maxEntriesPerBlock = (BLOCK_SIZE - sizeof(int) - sizeof(bool)) / (ENTRY_SIZE + sizeof(int));
-// 512 - 4 - 1 = 507
-// 507 / 33 = 15
+#include <ctime>
 
-struct GameEntry // 29 bytes
+struct GameEntry // 33 bytes
 {
-    std::time_t GAME_DATE_EST;
-    int TEAM_ID_home;    // 4 bytes
-    int PTS_home;        // 4 bytes
-    float FG_PCT_home;   // 4 bytes
-    float FT_PCT_home;   // 4 bytes
-    float FG3_PCT_home;  // 4 bytes
-    int AST_home;        // 4 bytes
-    int REB_home;        // 4 bytes
-    bool HOME_TEAM_WINS; // 1 byte
+    std::time_t GAME_DATE_EST; // 8 bytes
+    int TEAM_ID_home;          // 4 bytes
+    int PTS_home;              // 4 bytes
+    float FG_PCT_home;         // 4 bytes
+    float FT_PCT_home;         // 4 bytes
+    float FG3_PCT_home;        // 4 bytes
+    int AST_home;              // 4 bytes
+    int REB_home;              // 4 bytes
+    bool HOME_TEAM_WINS;       // 1 byte
 };
 
+const int BLOCK_SIZE = 512;
+const int ENTRY_SIZE = sizeof(GameEntry) + sizeof(int); // 33 + 4 = 37
+const int maxEntriesPerBlock = (BLOCK_SIZE - sizeof(int) - sizeof(bool)) / (ENTRY_SIZE + sizeof(int));
+// 512 - 4 - 1 = 507
+// 507 / 37 = 13
 struct Node
 {
     int n;
-    GameEntry entries[maxEntriesPerBlock];
-    Node *children[maxEntriesPerBlock + 1];
+    GameEntry entries[13];  // maxEntriesPerBlock
+    Node *children[13 + 1]; // maxEntriesPerBlock + 1
     bool leaf;
 };
 
